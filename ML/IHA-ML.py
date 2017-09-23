c4199cb52d8c2283c0cfa278664bb79a50c55870
jmolives <jeanmichealolivas55522@yahoo.com>
2017-09-24 00:51:12 +0800

    First Commit
    IHA Machine Learning Base

---
+++ ML/IHA-ML.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 15:25:06 2017
+
+@author: jm
+"""
+import speech_recognition as sr
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+import string
+import re
+import spacy
+from spacy.en import English
+STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "ca"] + list(ENGLISH_STOP_WORDS))
+# List of symbols we don't care about
+SYMBOLS = " ".join(string.punctuation).split(" ") + ["-----", "---", "...", "“", "”", "'ve"]
+class CleanTextTransformer(TransformerMixin):
+    """
+    Convert text to cleaned text
+    """
+
+    def transform(self, X, **transform_params):
+        return [cleanText(text) for text in X]
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+def cleanText(text):
+    # get rid of newlines
+        text = text.strip().replace("\n", " ").replace("\r", " ")
+    
+    # replace twitter @mentions
+        mentionFinder = re.compile(r"@[a-z0-9_]{1,15}", re.IGNORECASE)
+        text = mentionFinder.sub("@MENTION", text)
+    
+    # replace HTML symbols
+        text = text.replace("&amp;", "and").replace("&gt;", ">").replace("&lt;", "<")
+    
+    # lowercase
+        text = text.lower()
+
+        return text
+    
+# A custom function to tokenize the text using spaCy
+# and convert to lemmas
+def tokenizeText(sample):
+
+    # get the tokens using spaCy
+    tokens = parser(sample)
+
+    # lemmatize
+    lemmas = []
+    for tok in tokens:
+        lemmas.append(tok.lemma_.lower().strip() if tok.lemma_ != "-PRON-" else tok.lower_)
+    tokens = lemmas
+
+    # stoplist the tokens
+    tokens = [tok for tok in tokens if tok not in STOPLIST]
+
+    # stoplist symbols
+    tokens = [tok for tok in tokens if tok not in SYMBOLS]
+
+    # remove large strings of whitespace
+    while "" in tokens:
+        tokens.remove("")
+    while " " in tokens:
+        tokens.remove(" ")
+    while "\n" in tokens:
+        tokens.remove("\n")
+    while "\n\n" in tokens:
+        tokens.remove("\n\n")
+
+    return tokens
+
+def Lights_On(location):
+   def allLights():
+       print("All lights are turned on")
+   def kitchenLights():
+       print("Kitchen lights are turned on")
+   def livingRoomLights():
+       print("Living room lights are turned on")
+   case={"all":allLights,
+         "kitchen":kitchenLights,
+         "livingRoom":livingRoomLights}
+   case[location]()
+def Lights_Off(location):
+   def allLights():
+       print("All lights are turned off")
+   def kitchenLights():
+       print("Kitchen lights are turned off")
+   def livingRoomLights():
+       print("Living room lights are turned off")
+   case={"all":allLights,
+         "kitchen":kitchenLights,
+         "livingRoom":livingRoomLights}
+   case[location]()
+        
+nlp=spacy.load('en')
+vectorizer = CountVectorizer(tokenizer=tokenizeText, ngram_range=(1,1))
+clf = LinearSVC()
+pipeline=Pipeline([("cleanText",CleanTextTransformer()),("vectorizer",vectorizer),("clf",clf)])
+#commandsDataSet
+commands = ["turn off the lights","turn off the lights in the living room","turn off the lights in the kitchen","turn off the television","turn on the lights","turn on all the lights", "turn on the lights in the kitchen","turn the lights on","turn off all the lights","turn the lights off"]
+commandLabel = ["lights-all=off", "lights-livingRoom=off", "lights-kitchen=off", "television-livingRoom=off", "lights-all=on", "lights-all=on", "lights-kitchen=on","lights-all=on","lights-all=off","lights-all=off"]
+#train
+pipeline.fit(commands, commandLabel)
+#speech to text
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source:                                                                       
+    print("Speak:")                                                                                   
+    audio = r.listen(source)   
+
+    speech = [r.recognize_google(audio)]
+    
+#Predict
+preds=pipeline.predict(speech)
+predString=str(preds)
+predString=predString[2:len(predString)-2]
+#Get intent
+intent=predString.split('-')[0]
+#Get location
+location=predString.split('-')[1].split('=')[0]
+#Get Action
+act=predString.split('-')[1].split('=')[1]
+if intent=="lights":
+    action={"on":Lights_On,
+            "off":Lights_Off
+            }
+    action[act](location)
+if intent=="television":
+    print("tv")
+"""
+for(sample,pred) in zip(test,preds):
+   print(sample, ":", pred) 
+"""
+
+   
+       
+
